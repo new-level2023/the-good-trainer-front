ddd8283c60e05f8f69a646abbb2521b2262d4906
Alexis Recalde <alexis.recalde@hotmail.com>
2023-10-20 00:15:29 -0300

    fix

--- android/app/src/main/java/com/thegoodtrainer/MainActivity.java
+++ android/app/src/main/java/com/thegoodtrainer/MainActivity.java
@@ -4,7 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
+//import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
 public class MainActivity extends ReactActivity {
 
